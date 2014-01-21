ff9c61378aaa2ba690b24afe4fec527a56e775bd
Thomas Chauvet <thomaschvt@gmail.com>
2014-01-21 20:44:13 +0100

    no message

---
+++ translation/en.php
@@ -0,0 +1,10 @@
+<?php
+/*Fichier de traduction en*/
+
+$heritage = strtoupper("Legacy");
+$contact = "Nous contacter";
+$trouver = "Nous trouver";
+$telechargement_images = "Télécharger les images";
+
+?>
+
---
+++ translation/fr.php
@@ -0,0 +1,9 @@
+<?php
+/*Fichier de traduction fr*/
+
+$heritage = "l'héritage";
+$contact = "Nous contacter";
+$trouver = "Nous trouver";
+$telechargement_images = "Télécharger les images";
+
+?>
